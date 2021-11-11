1c1b6f2298cf299aec4b5607cb13f8fa80e6bfde
Caglan Atay <caglanatay@windowslive.com>
2021-11-11 19:38:55 +0300

    3.37 Triggers To Restart Level

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Ground")
+    {
+      Debug.Log("Ouch, hit my head");
+    }
+
+  }
+}
---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Player")
+    {
+      Debug.Log("You finished");
+    }
+
+  }
+}
